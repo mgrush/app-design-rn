c25c2347c7d61c4932a671ac3c1041835e266b58
yuquan <yuquan@wacai.com>
2018-10-02 20:23:50 +0800

    update entry index

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+
 export { default as RootView } from './src/RootView' 
-export { default as Modal } from './src/Modal'
+
+export { default as Modal } from './src/Modal/Modal'
+export { default as Dialog } from './src/Modal/Dialog'
+export { default as Popup } from './src/Modal/Popup'
