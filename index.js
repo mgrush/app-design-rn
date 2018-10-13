659bdcdc271fe821e230117b3ee2308102272822
yuquan <yuquan@wacai.com>
2018-10-13 15:28:37 +0800

    增加渐隐动画容器组件

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 export { default as RootView } from './src/RootView' 
 
+// 模态弹窗组件
 export { default as Modal } from './src/Modal/Modal'
 export { default as Dialog } from './src/Modal/Dialog'
 export { default as Popup } from './src/Modal/Popup'
 export { default as Popover } from './src/Modal/Popover'
 
+// 动画容器组件
+export { default as OpacityUIAnimation } from './src/UIAnimation/OpacityUIAnimation'
 export { default as FadeUIAnimation } from './src/UIAnimation/FadeUIAnimation'
 export { default as SlideUIAnimation } from './src/UIAnimation/SlideUIAnimation'
